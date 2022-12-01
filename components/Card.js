2416da7e3a5170bcf19de8b8720c4f22a782b964
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2022-12-01 12:06:10 +0100

    add remove button with logic

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import { CiCircleRemove } from "react-icons";
 
-export default function Card({ name, thoughts }) {
+export default function Card({ name, thoughts, id, onRemoveCard }) {
   return (
     <StyledCard>
       <h4>{name} is thinking about...</h4>
       <p>{thoughts}</p>
+      <button onClick={() => onRemoveCard(id)}>remove</button>
     </StyledCard>
   );
 }
